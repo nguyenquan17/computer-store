48e1ec301e4e99b66702b10dd8892d38962c1578
nguyenquan17 <quannguyen2213@gmail.com>
2023-05-04 20:12:53 +0700

    sua footer

--- src/components/layout/layout-landing/Footer.vue
+++ src/components/layout/layout-landing/Footer.vue
@@ -1,96 +1,96 @@
 <template>
   <div>
-    <div class="border-solid border-t-[3px] border-secondary">
+    <div class="border-t-[3px] border-solid border-secondary">
       <div class="py-4">
-        <div class="w-full m-auto px-4 flex mb-[10px] max-w-[1264px]">
+        <div class="m-auto mb-[10px] flex w-full max-w-[1264px] px-4">
           <div class="flex w-full justify-between">
             <div class="flex flex-col">
-              <div class="text-sm font-bold mb-[10px]">Hỗ trợ khách hàng</div>
-              <div class="text-xs flex flex-col">
-                <a href="" class="pt-2 pr-3">Thẻ ưu đãi</a>
-                <a href="" class="pt-2 pr-3">Hướng dẫn mua online</a>
-                <a href="" class="pt-2 pr-3">Ưu đãi dành cho Doanh nghiệp</a>
-                <a href="" class="pt-2 pr-3">Chính sách trả góp</a>
+              <div class="mb-[10px] text-sm font-bold">Hỗ trợ khách hàng</div>
+              <div class="flex flex-col text-xs">
+                <a class="pt-2 pr-3" href="">Thẻ ưu đãi</a>
+                <a class="pt-2 pr-3" href="">Hướng dẫn mua online</a>
+                <a class="pt-2 pr-3" href="">Ưu đãi dành cho Doanh nghiệp</a>
+                <a class="pt-2 pr-3" href="">Chính sách trả góp</a>
               </div>
             </div>
             <div class="flex flex-col">
-              <div class="text-sm font-bold mb-[10px]">Chính sách mua hàng</div>
-              <div class="text-xs flex flex-col">
-                <a href="" class="pt-2 pr-3">Điều kiện giao dịch chung</a>
-                <a href="" class="pt-2 pr-3">Chính sách bảo hành</a>
-                <a href="" class="pt-2 pr-3">Chính sách đổi trả</a>
-                <a href="" class="pt-2 pr-3">Chính sách thanh toán</a>
-                <a href="" class="pt-2 pr-3">Giao hàng và Lắp đặt tại nhà</a>
-                <a href="" class="pt-2 pr-3">Dịch vụ lắp đặt và nâng cấp PC/ Laptop tại cửa hàng & TTBH</a>
+              <div class="mb-[10px] text-sm font-bold">Chính sách mua hàng</div>
+              <div class="flex flex-col text-xs">
+                <a class="pt-2 pr-3" href="">Điều kiện giao dịch chung</a>
+                <a class="pt-2 pr-3" href="">Chính sách bảo hành</a>
+                <a class="pt-2 pr-3" href="">Chính sách đổi trả</a>
+                <a class="pt-2 pr-3" href="">Chính sách thanh toán</a>
+                <a class="pt-2 pr-3" href="">Giao hàng và Lắp đặt tại nhà</a>
+                <a class="pt-2 pr-3" href="">Dịch vụ lắp đặt và nâng cấp PC/ Laptop tại cửa hàng & TTBH</a>
               </div>
             </div>
             <div class="flex flex-col">
-              <div class="text-sm font-bold mb-[10px]">Thông tin Quân Lộc</div>
-              <div class="text-xs flex flex-col">
-                <a href="" class="pt-2 pr-3">Giới thiệu Quân Lộc</a>
-                <a href="" class="pt-2 pr-3">Hệ thống cửa hàng</a>
-                <a href="" class="pt-2 pr-3">Trung tâm bảo hành</a>
-                <a href="" class="pt-2 pr-3">Chính sách bảo mật</a>
-                <a href="" class="pt-2 pr-3">Tin công nghệ</a>
-                <a href="" class="pt-2 pr-3">Hỏi đáp</a>
-                <a href="" class="pt-2 pr-3">Tuyển dụng</a>
+              <div class="mb-[10px] text-sm font-bold">Thông tin Quân Lộc</div>
+              <div class="flex flex-col text-xs">
+                <a class="pt-2 pr-3" href="">Giới thiệu Quân Lộc</a>
+                <a class="pt-2 pr-3" href="">Hệ thống cửa hàng</a>
+                <a class="pt-2 pr-3" href="">Trung tâm bảo hành</a>
+                <a class="pt-2 pr-3" href="">Chính sách bảo mật</a>
+                <a class="pt-2 pr-3" href="">Tin công nghệ</a>
+                <a class="pt-2 pr-3" href="">Hỏi đáp</a>
+                <a class="pt-2 pr-3" href="">Tuyển dụng</a>
               </div>
             </div>
             <div class="flex flex-col">
-              <div class="text-sm font-bold mb-[10px]">Cộng đồng Quân Lộc</div>
-              <div class="text-xs flex flex-col">
+              <div class="mb-[10px] text-sm font-bold">Cộng đồng Quân Lộc</div>
+              <div class="flex flex-col text-xs">
                 <div class="pt-2 pr-3">
                   Gọi mua hàng (miễn phí)
-                  <a href="tel:18000018" class="text-hyperlink">18000018</a>
+                  <a class="text-hyperlink" href="tel:18000018">18000018</a>
                 </div>
                 <div class="pt-2 pr-3">
                   Gọi chăm sóc
-                  <a href="tel:18000018" class="text-hyperlink">18000018</a>
+                  <a class="text-hyperlink" href="tel:18000018">18000018</a>
                 </div>
-                <a href="" class="flex items-center pt-2 pr-3">
+                <a class="flex items-center pt-2 pr-3" href="">
                   <base-icon icon="icons8-facebook"></base-icon>
                   <div class="ml-1">Facebook Quân Lộc</div>
                 </a>
-                <a href="" class="flex items-center pt-2 pr-3">
+                <a class="flex items-center pt-2 pr-3" href="">
                   <base-icon icon="icons8-youtube"></base-icon>
                   <div class="ml-1">Media Quân Lộc</div>
                 </a>
-                <a href="" class="flex items-center pt-2 pr-3">
+                <a class="flex items-center pt-2 pr-3" href="">
                   <base-icon icon="icons8-telegram-app"></base-icon>
                   <div class="ml-1">Telegram Quân Lộc</div>
                 </a>
-                <a href="" class="flex items-center pt-2 pr-3">
+                <a class="flex items-center pt-2 pr-3" href="">
                   <base-icon icon="icons8-tiktok"></base-icon>
                   <div class="ml-1">Tiktok Quân Lộc</div>
                 </a>
               </div>
             </div>
             <div class="flex flex-col">
-              <div class="text-sm font-bold mb-[10px]">Email liên hệ</div>
-              <div class="text-xs flex-col">
+              <div class="mb-[10px] text-sm font-bold">Email liên hệ</div>
+              <div class="flex-col text-xs">
                 <div class="pt-2 pr-3">
                   Hỗ trợ Khách hàng:
-                  <br>
-                  <a href="mailto:quanloc@gmail.com" class="text-hyperlink">quanloc@gmail.com</a>
+                  <br />
+                  <a class="text-hyperlink" href="mailto:quanloc@gmail.com">quanloc@gmail.com</a>
                 </div>
                 <div class="pt-2 pr-3">
                   Liên hệ báo giá:
-                  <br>
-                  <a href="mailto:quanloc@gmail.com" class="text-hyperlink">quanloc@gmail.com</a>
+                  <br />
+                  <a class="text-hyperlink" href="mailto:quanloc@gmail.com">quanloc@gmail.com</a>
                 </div>
                 <div class="pt-2 pr-3">
                   Hợp tác phát triển:
-                  <br>
-                  <a href="mailto:quanloc@gmail.com" class="text-hyperlink">quanloc@gmail.com</a>
+                  <br />
+                  <a class="text-hyperlink" href="mailto:quanloc@gmail.com">quanloc@gmail.com</a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="w-full m-auto px-4 flex mb-[10px] max-w-[1264px]">
+        <div class="m-auto mb-[10px] flex w-full max-w-[1264px] px-4">
           <div class="flex w-[400px]">
             <div class="flex flex-col">
-              <h3 class="text-sm font-bold mb-4">Phương thức thanh toán</h3>
+              <h3 class="mb-4 text-sm font-bold">Phương thức thanh toán</h3>
               <div class="flex items-center gap-x-[20px]">
                 <div class="flex flex-col">
                   <base-icon icon="qrcode" size="36"></base-icon>
@@ -112,27 +112,27 @@
             </div>
           </div>
           <div class="flex flex-col">
-            <h3 class="text-sm font-bold mb-4">Danh sách các ngân hàng thanh toán online</h3>
-            <img src="https://shopfront-cdn.tekoapis.com/static/vnpay_banks.png" alt="">
+            <h3 class="mb-4 text-sm font-bold">Danh sách các ngân hàng thanh toán online</h3>
+            <img alt="" src="https://shopfront-cdn.tekoapis.com/static/vnpay_banks.png" />
           </div>
         </div>
       </div>
-      <div class="py-[30px]">
-        <div class="w-full m-auto max-w-[1264px] px-4 py-4 flex">
-          <div class="w-ful flex justify-between flex-wrap gap-x-[20px]">
+      <div class="bg-[#e9edf2] py-[30px]">
+        <div class="m-auto flex w-full max-w-[1264px] px-4 py-4">
+          <div class="w-ful flex flex-wrap justify-between gap-x-[20px]">
             <div class="left flex-1">
-              <h2 class="text-sm font-bold mb-4">Công ty cổ phần thương mại - dịch vụ Phong Vũ</h2>
+              <h2 class="mb-4 text-sm font-bold">Công ty cổ phần thương mại - dịch vụ Quân Lộc</h2>
               <div class="text-xs">
-                © 1997 - 2023 Công Ty Cổ Phần Thương Mại - Dịch Vụ Quân Lộc
-                <br>
-                Giấy chứng nhận đăng ký doanh nghiệp: 0304998358 do Sở KH-ĐT TP.HCM cấp lần đầu ngày 30 tháng 05 năm 2007
+                © 2022 - 2023 Công Ty Cổ Phần Thương Mại - Dịch Vụ Quân Lộc
+                <br />
+                Giấy chứng nhận đăng ký doanh nghiệp: 0304998358 do Sở KH-ĐT TP.HCM cấp lần đầu ngày 30 tháng 05 năm 2022
               </div>
             </div>
             <div class="align">
-             <div class="text-xs">
-               <h3 class="font-bold">Địa chỉ trụ sở chính:</h3>
-               <p>Tầng 5, Số 117-119-121 Nguyễn Du, Phường Bến Thành, Quận 1, Thành Phố Hồ Chí Minh</p>
-             </div>
+              <div class="text-xs">
+                <h3 class="font-bold">Địa chỉ trụ sở chính:</h3>
+                <p>Tầng 5, Số 117-119-121 Nguyễn Du, Phường Bến Thành, Quận 1, Thành Phố Hồ Chí Minh</p>
+              </div>
               <div class="text-xs">
                 <h3 class="font-bold">Văn phòng điều hành miền Bắc:</h3>
                 <p>Tầng 6, Số 1 Phố Thái Hà, Phường Trung Liệt, Quận Đống Đa, Hà Nội</p>
@@ -143,8 +143,11 @@
               </div>
             </div>
             <div class="right">
-              <img src="https://shopfront-cdn.tekoapis.com/common/da-dang-ky.png" alt="">
-              <img src="https://images.dmca.com/Badges/dmca-badge-w100-2x1-02.png?ID=53b44883-ed2a-434d-902b-5adce10aafd5" alt="">
+              <img alt="" src="https://shopfront-cdn.tekoapis.com/common/da-dang-ky.png" />
+              <img
+                alt=""
+                src="https://images.dmca.com/Badges/dmca-badge-w100-2x1-02.png?ID=53b44883-ed2a-434d-902b-5adce10aafd5"
+              />
             </div>
           </div>
         </div>
